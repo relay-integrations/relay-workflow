af216cf80cd955dcdd74ca5b1e353f763df1408f
Dimitri Tischenko <dimitri@puppet.com>
2022-05-16 15:56:46 +0200

    Fix review comments

--- steps/run/step.py
+++ steps/run/step.py
@@ -1,27 +1,27 @@
 import logging
-from relay_sdk import Interface, Dynamic as D
+from relay_sdk import Interface, Dynamic as D, UnresolvableException
 
 relay = Interface()
 
 name = relay.get(D.name)
 
 params = None
-params_array = None
+param_sets = None
 
-# if `parameters` is defined, `parameters_array` is ignored
+# if `parameters` is defined, `parameterSets` is ignored
 try:
-  params = relay.get(D.parameters)
-except:
-  try:
-    params_array = relay.get(D.parameters_array)
-  except:
-    params = {}
+    params = relay.get(D.parameters)
+except UnresolvableException:
+    try:
+        param_sets = relay.get(D.parameterSets)
+    except UnresolvableException:
+        params = {}
 
-if params_array:
-  logging.debug(f'Iterating workflow {name} over params array {params_array}')
-  #TODO some throttling or limitation
-  for params in params_array:
-    relay.workflows.run(name, params)
+if param_sets:
+    logging.debug(
+        f'Iterating workflow {name} over params array {param_sets}')
+    for params in param_sets:
+        relay.workflows.run(name, params)
 else:
     logging.debug(f'Running workflow {name} with params {params}')
     relay.workflows.run(name, params)
