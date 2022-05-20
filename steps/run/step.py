7610fbf28d0fc26054d343f4185a053c5e96994c
Dimitri Tischenko <dimitri@puppet.com>
2022-05-20 18:17:01 +0200

    Add decorators

--- steps/run/step.py
+++ steps/run/step.py
@@ -1,4 +1,5 @@
 import logging
+import json
 from relay_sdk import Interface, Dynamic as D
 
 relay = Interface()
@@ -18,10 +19,25 @@ except OSError:
         params = {}
 
 if param_sets:
-    logging.debug(
+    logging.info(
         f'Iterating workflow {name} over params array {param_sets}')
     for params in param_sets:
-        relay.workflows.run(name, params)
+        resp = relay.workflows.run(name, params)
+        logging.info(json.dumps(resp, indent=2))
+        run = resp['workflow_run']
+        relay.decorators.set_link(
+            f'{run["run_number"]:03d}', # add leading zeros for proper ordering in the UI # noqa E501
+            f'Run {run["run_number"]:03d}',
+            run['app_url']
+        )
+
 else:
-    logging.debug(f'Running workflow {name} with params {params}')
-    relay.workflows.run(name, params)
+    logging.info(f'Running workflow {name} with params {params}')
+    resp = relay.workflows.run(name, params)
+    logging.info(json.dumps(resp, indent=2))
+    run = resp['workflow_run']
+    relay.decorators.set_link(
+        f'{run["run_number"]:03d}', # add leading zeros for proper ordering in the UI # noqa E501
+        f'Run {run["run_number"]:03d}',
+        run['app_url']
+        )
